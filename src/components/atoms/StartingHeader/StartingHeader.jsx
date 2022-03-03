8fca6fb9eea690f70840bf96aa3591f742e5cdd2
Vitoldas <vitoldascebatorius@gmail.com>
2022-03-03 16:54:08 +0200

    update data output

--- src/components/atoms/StartingHeader/StartingHeader.jsx
+++ src/components/atoms/StartingHeader/StartingHeader.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import './startingHeader.scss';
 
 function StartingHeader() {
+  const startingTemplate = 'Starting template';
   return (
     <div>
-      <h1 className="starting-header__h1">Starting template</h1>
+      <h1 className="starting-header__h1">{startingTemplate}</h1>
     </div>
   );
 }
